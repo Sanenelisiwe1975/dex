b948592208cc5cf8e7293cfb79ef66990d2ac0ed
sasimatjhb024 <sasimatjhb024@student.wethinkcode.co.za>
2025-09-15 11:58:36 +0200

    coke token

--- contracts/coke.sol
+++ contracts/coke.sol
@@ -1,13 +1,12 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";  // OpenZeppelin package contains implementation of the ERC 20 standard, which our NFT smart contract will inherit
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract COKE is ERC20 {
-    uint constant _initial_supply = 100 * (10**18);  // setting variable for how many of your own tokens are initially put into your wallet, feel free to edit the first number but make sure to leave the second number because we want to make sure our supply has 18 decimals
+contract Coke is ERC20 {
+    uint256 constant _initial_supply = 100 * 10**18;
 
-    /* ERC 20 constructor takes in 2 strings, feel free to change the first string to the name of your token name, and the second string to the corresponding symbol for your custom token name */
-    constructor() ERC20("Coke", "CK") public {
+    constructor() ERC20("Coke", "CK") {
         _mint(msg.sender, _initial_supply);
     }
 }
